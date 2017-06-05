394d5272a854bc73cc8d0fa6f56987f461064aab
Andrii Kobzar <andrykobzar@gmail.com>
2017-06-05 22:18:23 +0300

    adding urls in api for competitors

--- Task9/backend/poll_backend/polls/urls.py
+++ Task9/backend/poll_backend/polls/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from polls.views import categories, competitions
+from polls.views import categories, competitions, competitors
 
 urlpatterns = [
     url(r'^categories/all', categories.get_categories),
     url(r'^categories/create', categories.create_category),
     url(r'^competitions/create', competitions.create_competition),
-    url(r'^competitions/all', competitions.get_competitions)
+    url(r'^competitions/all', competitions.get_competitions),
+    url(r'^competitors/all', competitors.get_competitors),
+    url(r'^competitors/create', competitors.create_competitor)
 ]
