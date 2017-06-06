f48bc4e30a87dfdc84cd91e94b8d8203e84f2770
Andrii Kobzar <andrykobzar@gmail.com>
2017-06-06 21:51:05 +0300

    added api for voting

---
+++ Task9/backend/poll_backend/polls/views/vote.py
@@ -0,0 +1,37 @@
+import json
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from polls.models import Vote, Category, User, Competition
+from django.db.models import Count
+
+
+# {categoryId:<id>, competitorId:<compId>}
+@csrf_exempt
+def vote(request):
+    user = User(vkId="qwe")
+    user.save()
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    category = Category.objects.get(pk=body['categoryId'])
+    competitor = Category.objects.get(pk=body['competitorId'])
+    vote = Vote()
+    vote.category = category
+    vote.competitor = competitor
+    vote.user = user
+    vote.save()
+    return HttpResponse("{result:'ok'}", content_type="application/json")
+
+
+def get_votes_for_category(request):
+    categoryId = int(request.GET.get('categoryId', -1))
+    category = Category.objects.get(pk=categoryId)
+    votes = Vote.objects.all().filter(category=category)
+    votes_map = votes.objects.annotate(votes=Count('competitor'))
+
+    return HttpResponse(json.dumps(votes_map), content_type="application/json")
+
+
+def map_votes(votes):
+    return votes.objects.annotate()
